2ca405493483259fd24b58ade02968a24649af49
Lester Marroquin <lester.marroquin96@gmail.com>
2023-06-29 16:42:16 -0600

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,31 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+};
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completada o pendiente la tarea'
+};
+
+
+
+const argv = require ('yargs')
+.command('crear', 'Crea una tarea por realizar', {
+    descripcion
+})
+.command('actualizar', 'Actualiza el estado de la tareas', {
+    descripcion,
+    completado
+})
+.command('borrar', 'Borra una tarea', {
+    descripcion
+})
+.help()
+.argv;
+
+module.exports = {
+    argv
+}
